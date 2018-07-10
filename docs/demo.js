e8c2c802626201d642ee2e3905a0bd3ef42a22cf
Thomas Scheibitz <thomas.scheibitz@in2code.de>
2018-07-10 11:23:23 +0200

    [TASK] add reset cookie for demo

---
+++ docs/demo.js
@@ -0,0 +1,1 @@
+function resetCookie(){setBodyBottomPadding();var e=new Date;e.setTime(e.getTime()+864e5),document.cookie=cookieName+"=;path=/;expires="+e.toGMTString(),$cookieMessagebar.classList.add(cookieMessageSwitchClassname)}
---
+++ source/javascripts/demo.js
@@ -0,0 +1,7 @@
+function resetCookie() {
+	setBodyBottomPadding();
+	var d = new Date();
+	d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+	document.cookie = cookieName + '=' + '' + ';path=/;expires=' + d.toGMTString();
+	$cookieMessagebar.classList.add(cookieMessageSwitchClassname);
+}
